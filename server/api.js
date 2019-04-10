082962ebd0d429ab54eef0d6dec28606a226c0c2
jetmech <josephlesage84@gmail.com>
2019-04-10 12:18:53 +1000

    Add router for /api/meetings

--- server/api.js
+++ server/api.js
@@ -2,8 +2,10 @@ const express = require('express');
 const apiRouter = express.Router();
 const minionsRouter = require('./api/minions');
 const ideasRouter = require('./api/ideas');
+const meetingsRouter = require('./api/meetings');
 
 apiRouter.use('/minions', minionsRouter);
 apiRouter.use('/ideas', ideasRouter);
+apiRouter.use('/meetings', meetingsRouter);
 
 module.exports = apiRouter;
