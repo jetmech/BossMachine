5121c13e4314917db992e401094e20b6e1094824
jetmech <josephlesage84@gmail.com>
2019-04-10 10:17:39 +1000

    Mount ideas router

--- server/api.js
+++ server/api.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const apiRouter = express.Router();
 const minionsRouter = require('./api/minions');
+const ideasRouter = require('./api/ideas');
 
 apiRouter.use('/minions', minionsRouter);
+apiRouter.use('/ideas', ideasRouter);
 
 module.exports = apiRouter;
