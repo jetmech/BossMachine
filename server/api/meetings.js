fba935a6fe876acafc076fa937d26176b20907ff
jetmech <josephlesage84@gmail.com>
2019-04-10 12:41:01 +1000

    Fix POST /api/meetings/

--- server/api/meetings.js
+++ server/api/meetings.js
@@ -7,27 +7,15 @@ meetingsRouter.get('/', (req, res, next) => {
 });
 
 meetingsRouter.post('/', (req, res, next) => {
-  let meeting = req.body;
-  if(validMeeting(meeting)) {
-    let newMeeting = db.addToDatabase('meetings', meeting);
-    res.status(201).send(newMeeting);
-  } else {
-    res.status(404).send();
-  }
+  let meeting = db.createMeeting();
+
+  let newMeeting = db.addToDatabase('meetings', meeting);
+  res.status(201).send(newMeeting);
 });
 
 meetingsRouter.delete('/', (req, res, next) => {
   db.deleteAllFromDatabase('meetings');
   res.status(204).send();
 });
-
-function validMeeting(meeting) {
-  let validTime = typeof meeting.time === 'string';
-  let validDate = '';
-  let validTitle = typeof meeting.title === 'string';
-  let validSalary = typeof meeting.salary === 'number';
-
-  return validTime && validTitle && validSalary;
-}
 
 module.exports = meetingsRouter;
