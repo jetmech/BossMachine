9b878f0969316fba0edc14c579c1e21377920f2a
jetmech <josephlesage84@gmail.com>
2019-04-10 11:51:34 +1000

    Worte checkMillionDollarIdea function

--- server/checkMillionDollarIdea.js
+++ server/checkMillionDollarIdea.js
@@ -1,4 +1,19 @@
-const checkMillionDollarIdea = () => {};
+const checkMillionDollarIdea = (req, res, next) => {
+  let totalIncome;
+  let {weeklyRevenue, numWeeks} = req.body;  
+
+  if (typeof numWeeks !== 'number' || typeof weeklyRevenue !== 'number') {
+    return res.status(400).send();
+  } else {
+    totalIncome = numWeeks * weeklyRevenue;
+  }
+
+  if (totalIncome < 1000000) {
+    res.status(400).send();
+  } else {
+    return next();
+  }
+};
 
 // Leave this exports assignment so that the function can be used elsewhere
 module.exports = checkMillionDollarIdea;
