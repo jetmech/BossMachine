eb49611eecc36ca9bc4a2a3fa3f80cecee48de2c
jetmech <josephlesage84@gmail.com>
2019-04-10 10:18:16 +1000

    Add ideas router routes

---
+++ server/api/ideas.js
@@ -0,0 +1,61 @@
+const ideasRouter = require('express').Router();
+const db = require('../db');
+
+ideasRouter.param('ideaId', (req, res, next, id) => {
+  let idea = {};
+  if (id) {
+    idea = db.getFromDatabaseById('ideas', id);
+  } else {
+    res.status(400).send();
+  }
+
+  if (idea) {
+    req.idea = idea;
+    next();
+  } else {
+    res.status(404).send();
+  }
+});
+
+ideasRouter.get('/', (req, res, next) => {
+  const ideas = db.getAllFromDatabase('ideas');
+  res.send(ideas);
+});
+
+ideasRouter.get('/:ideaId', (req, res, next) => {
+  res.send(req.idea);
+});
+
+ideasRouter.put('/:ideaId', (req, res, next) => {
+  const newIdea = db.updateInstanceInDatabase('ideas', req.body);
+  res.send(newIdea);
+});
+
+ideasRouter.post('/', (req, res, next) => {
+  let idea = req.body;
+  if(validIdea(idea)) {
+    let newIdea = db.addToDatabase('ideas', idea);
+    res.status(201).send(newIdea);
+  } else {
+    res.status(404).send();
+  }
+});
+
+ideasRouter.delete('/:ideaId', (req, res, next) => {
+  if(db.deleteFromDatabasebyId('ideas', req.idea.id)) {
+    res.status(204).send();
+  } else {
+    res.status(404).send();
+  }
+});
+
+function validIdea(idea) {
+  let validName = typeof idea.name === 'string';
+  let validDescription = typeof idea.description === 'string';
+  let validNumWeeks = typeof idea.numWeeks === 'number';
+  let validWeeklyRevenue = typeof idea.weeklyRevenue === 'number';
+
+  return validName && validDescription && validNumWeeks && validWeeklyRevenue;
+}
+
+module.exports = ideasRouter;
